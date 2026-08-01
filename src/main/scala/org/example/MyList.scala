d7e284160410716d2b17e95a570fd0d931884c56
mateusz <mzalewski1992@gmail.com>
2026-08-02 00:43:53 +0200

    use case class and object

--- src/main/scala/org/example/MyList.scala
+++ src/main/scala/org/example/MyList.scala
@@ -23,7 +23,7 @@ abstract class MyList[+A] {
   def ++[B >: A](list: MyList[B]): MyList[B]
 }
 
-object Empty extends MyList[Nothing] {
+case object Empty extends MyList[Nothing] {
 
   override def head: Nothing = throw new NoSuchElementException()
 
@@ -44,7 +44,7 @@ object Empty extends MyList[Nothing] {
   override def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
 }
 
-class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
 
   override def head: A = h
 
